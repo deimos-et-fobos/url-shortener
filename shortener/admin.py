fca405b7af0649643d357219d04d5275e3cf9124
deimos-et-fobos <ricardo.marquez.88@gmail.com>
2025-06-23 01:19:47 -0300

    chore: integrate cache to redirect view

--- shortener/admin.py
+++ shortener/admin.py
@@ -3,5 +3,6 @@ from .models import ShortURL
 
 class ShortURLAdmin(admin.ModelAdmin):
     readonly_fields = ['last_access']
+    list_filter = ['last_access']
 
 admin.site.register(ShortURL, ShortURLAdmin)
